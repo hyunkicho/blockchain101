ac162e9491d19e603dde5a2d6f7e9d88d9dfad63
hyunkicho <dmsrlgusrl@naver.com>
2023-01-01 17:48:16 +0900

    build: call과 send 각각 해보기

--- basics/tutorial_truffle/VendingMachine/scripts/cupcakeBalances.js
+++ basics/tutorial_truffle/VendingMachine/scripts/cupcakeBalances.js
@@ -7,19 +7,13 @@ const contractAddress = '0xFcD846259Ff8AE19d2B36F577039f8798DF278De';
 const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:9545/'));
 const contract = new web3.eth.Contract(contractABI, contractAddress);
 
-async function purchase (_from, _value) {
-  await contract.methods.purchase(1).send({from: _from, value: _value})
-  .on('transactionHash', function(hash){
-    console.log("tx hash >>", hash)
-  })
-  .on('receipt', function(receipt){
-    console.log("tx receipt >>", receipt)
-  })
-  .on('error', console.error);
+async function getBalance (_address) {
+  const balance = await contract.methods.cupcakeBalances(_address).call();
+  console.log("balance >>", balance);
 }
 
-purchase("0x3787879da0794418e4a41cb8bc5b9f78c2b7fb34", 1*(10**18))
+getBalance("0x3787879da0794418e4a41cb8bc5b9f78c2b7fb34")
 
 //STEP 01 : truffle develop에서 endpoint 확인
 //STEP 02 : truffle migrate 명령어를 통해 배포
-//SETP 03 : 배포된 컨트렉트에 상호작용 해보기 - send
+//SETP 03 : 배포된 컨트렉트에 상호작용 해보기 - call
