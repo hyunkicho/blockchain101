f978a0b9194b4c617d7772cdefd9ee7e5a12f422
hyunkicho <dmsrlgusrl@naver.com>
2023-01-04 23:25:27 +0900

    test: console.log 추가

--- basics/tutorial_hardhat/scripts/purchaseEvent.ts
+++ basics/tutorial_hardhat/scripts/purchaseEvent.ts
@@ -1,7 +1,7 @@
 import { ethers } from "hardhat";
 const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
-async function purchase​​(amount: number) {
+async function purchase(amount: number) {
   const VendingMachine = await ethers.getContractFactory("VendingMachine");
   const vendingMachine = await VendingMachine.attach(contractAddress);
   const filter: any = {
@@ -12,15 +12,17 @@ async function purchase​​(amount: number) {
   };
   const logs = await ethers.provider.getLogs(filter);
   //특정 이벤트만 필터링 하기 위한 로그 값
-  console.log("logs >>>", logs)
+  // console.log("logs >>>", logs)
   let abi = require("../artifacts/contracts/VendigMachine.sol/VendingMachine.json").abi;
   let iface = new ethers.utils.Interface(abi);
   //로그를 분석하기 위해서 abi를 가져옴
   logs.forEach(async(logs: any) => {
     //실제로 이벤트 로그 내용을 분석하기 위해서는 각각의 트랜잭션 receipt를 가져와서 처리해야 한다.
     const receipt = await ethers.provider.getTransactionReceipt(logs.transactionHash);
+    // console.log("receipt >>>", receipt);
     //반복문을 통해서 각로그들의 내용 출력 진행
     receipt.logs.forEach((log: any) => {
+      // console.log("iface.parseLog(log) >>", iface.parseLog(log));
       console.log("purchaser >>",iface.parseLog(log).args[0]);
       console.log("amount >>",iface.parseLog(log).args[1]);
     });
@@ -29,7 +31,7 @@ async function purchase​​(amount: number) {
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-purchase​​(10).catch((error) => {
+purchase(10).catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
