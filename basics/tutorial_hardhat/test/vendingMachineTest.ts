c35f4cfe5fe9d4cb8dce612faf7c414b956c4d1e
hyunkicho <dmsrlgusrl@naver.com>
2023-01-09 01:18:54 +0900

    fix: 불필요한 요소 제거

--- basics/tutorial_hardhat/test/vendingMachineTest.ts
+++ basics/tutorial_hardhat/test/vendingMachineTest.ts
@@ -1,8 +1,6 @@
-import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { VendingMachine } from '../typechain-types/VendigMachine.sol/VendingMachine';
 
 describe("Lock", function () {
   // We define a fixture to reuse the same setup in every test.
