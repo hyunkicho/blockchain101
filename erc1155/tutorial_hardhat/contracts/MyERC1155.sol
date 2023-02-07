80e6762d084d064206e5012ce67fbe549d3a38ed
hyunkicho <dmsrlgusrl@naver.com>
2023-02-08 00:12:20 +0900

    build: ERC1155 customize

--- erc1155/tutorial_hardhat/contracts/MyERC1155.sol
+++ erc1155/tutorial_hardhat/contracts/MyERC1155.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
-import "@openzeppelin/contracts/token/ERC1155/presets/ERC1155PresetMinterPauserAutoId.sol";
+import "@openzeppelin/contracts/token/ERC1155/presets/ERC1155PresetMinterPauser.sol";
 
 contract MyERC1155 is ERC1155PresetMinterPauser {
-    constructor() ERC721PresetMinterPauserAutoId("https://raw.githubusercontent.com/hyunkicho/blockchain101/main/erc1155/metadata/") {
-        mint(msg.sender);
+    constructor() ERC1155PresetMinterPauser("https://raw.githubusercontent.com/hyunkicho/blockchain101/main/erc1155/metadata/") {
+        mint(msg.sender,0,10,''); //data is needed only when neccessary
     }
 }
