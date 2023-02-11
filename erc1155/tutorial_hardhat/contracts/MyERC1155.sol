0c4e321935507f77f3f35202984218bd1faabae3
hyunkicho <dmsrlgusrl@naver.com>
2023-02-12 04:19:19 +0900

    fix: 예제에 맞게 수정

--- erc1155/tutorial_hardhat/contracts/MyERC1155.sol
+++ erc1155/tutorial_hardhat/contracts/MyERC1155.sol
@@ -3,9 +3,49 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/token/ERC1155/presets/ERC1155PresetMinterPauser.sol";
 
 contract MyERC1155 is ERC1155PresetMinterPauser {
-    constructor() ERC1155PresetMinterPauser("https://raw.githubusercontent.com/hyunkicho/blockchain101/main/erc1155/metadata/\{id\}.json") {
+    constructor() ERC1155PresetMinterPauser("https://raw.githubusercontent.com/hyunkicho/blockchain101/main/erc1155/metadata/") {
+        _setURI(0,'/series1/');
+        _setURI(0,'/series2/');
+        _setURI(0,'/series3/');
+
         mint(msg.sender,0,10,''); //data is needed only when neccessary
     }
 
-    
+    using Strings for uint256;
+
+    // Optional base URI
+    string private _baseURI = "";
+
+    // Optional mapping for token URIs
+    mapping(uint256 => string) private _tokenURIs;
+
+    function uri(uint256 tokenId) public view virtual override returns (string memory) {
+        string memory tokenURI = _tokenURIs[tokenId];
+
+        // If token URI is set, concatenate base URI and tokenURI (via abi.encodePacked).
+        return bytes(tokenURI).length > 0 ? string(abi.encodePacked(_baseURI, tokenURI)) : super.uri(tokenId);
+    }
+
+    function setURI(uint256 tokenId, string memory tokenURI) public {
+        _setURI(tokenId, tokenURI);
+    }
+
+    /**
+     * @dev Sets `tokenURI` as the tokenURI of `tokenId`.
+     */
+    function _setURI(uint256 tokenId, string memory tokenURI) internal virtual {
+        _tokenURIs[tokenId] = tokenURI;
+        emit URI(uri(tokenId), tokenId);
+    }
+
+    function setBaseURI(string memory baseURI) public {
+        _setBaseURI(baseURI);
+    }
+
+    /**
+     * @dev Sets `baseURI` as the `_baseURI` for all tokens
+     */
+    function _setBaseURI(string memory baseURI) internal virtual {
+        _baseURI = baseURI;
+    }
 }
