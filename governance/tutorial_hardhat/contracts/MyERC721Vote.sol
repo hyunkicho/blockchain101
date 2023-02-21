2160811d6eb0354095868a1116d3ad327b0db723
hyunkicho <dmsrlgusrl@naver.com>
2023-02-21 23:20:07 +0900

    build: initial commit

---
+++ governance/tutorial_hardhat/contracts/MyERC721Vote.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/draft-ERC721Votes.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract MyERC721Vote is ERC721, ERC721Burnable, Ownable, EIP712, ERC721Votes {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor(
+    ) ERC721("MyNFT", "MNFT") EIP712("MyToken", "1") {}
+    string private _baseTokenURI;
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return "https://raw.githubusercontent.com/hyunkicho/blockchain101/main/erc721/metadata/";
+    }
+
+    function mint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _afterTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+        internal
+        override(ERC721, ERC721Votes)
+    {
+        super._afterTokenTransfer(from, to, tokenId, batchSize);
+    }
+}
+
---
+++ governance/tutorial_hardhat/contracts/MyGovernor.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/governance/Governor.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
+
+contract MyGovernor is Governor, GovernorCountingSimple, GovernorVotes {
+    constructor(IVotes _token)
+        Governor("MyGovernor")
+        GovernorVotes(_token)
+    {}
+
+    function votingDelay() public pure override returns (uint256) {
+        return 9; // 9 block to snap shot
+    }
+
+    function votingPeriod() public pure override returns (uint256) {
+        return 5; // 5 block to vote
+    }
+
+    // // The following functions are overrides required by Solidity.
+    /**
+     * @dev Returns the quorum for a block number, in terms of number of votes: `supply * numerator / denominator`.
+     */
+    function quorum(uint256 blockNumber) public view virtual override returns (uint256) {
+        //원할 시 정족수에 대한 로직 추가 가능
+        return 2;
+    }
+
+    function quorumReached(uint256 proposalId) public view returns (bool){
+        return _quorumReached(proposalId);
+    }
+
+    function voteSucceeded(uint256 proposalId) public view returns (bool){
+        return _voteSucceeded(proposalId);
+    }
+
+}
