85c2bbb35f585a9062283b1541991964b9b4a2fa
hyunkicho <dmsrlgusrl@naver.com>
2023-03-11 14:54:22 +0900

    fix: fix comment

--- pausableAndReenterancy/tutorial_hardhat/scripts/deposit.ts
+++ pausableAndReenterancy/tutorial_hardhat/scripts/deposit.ts
@@ -1,15 +1,15 @@
 import { ethers } from "hardhat";
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { EtherStore } from '../typechain-types/contracts/EtherStore.sol/EtherStore';
-import { Contract } from 'ethers';
+import { Attack } from "../typechain-types";
 
 async function main() {
   const accounts = await ethers.getSigners();
   let alice:SignerWithAddress = accounts[0];
   let bob:SignerWithAddress = accounts[1];
   let eve:SignerWithAddress = accounts[2];
-  let etherStore:Contract;
-  let attack:Contract;
+  let etherStore:EtherStore;
+  let attack:Attack;
 
   const EtherStore = await ethers.getContractFactory("EtherStore");
   etherStore = await EtherStore.deploy();
@@ -26,11 +26,11 @@ async function main() {
   console.log("etherStore balance etherStore: ", await ethers.provider.getBalance(etherStore.address))
 
   attack.connect(eve).attack({value: (1*(10**18)).toString()})
-
-  console.log("after sending1 eth : ", await attack.getBalance());
-  console.log("after sending1 eth : attack ", await ethers.provider.getBalance(attack.address))
-  console.log("after sending1 eth : eve ", await ethers.provider.getBalance(eve.address))
-  console.log("etherStore balance : etherStore", await ethers.provider.getBalance(etherStore.address))
+//채굴전이므로 조회해봤자 업데이트 안됨
+//   console.log("after sending1 eth : ", await attack.getBalance());
+//   console.log("after sending1 eth : attack ", await ethers.provider.getBalance(attack.address))
+//   console.log("after sending1 eth : eve ", await ethers.provider.getBalance(eve.address))
+//   console.log("etherStore balance : etherStore", await ethers.provider.getBalance(etherStore.address))
 }
 
 // We recommend this pattern to be able to use async/await everywhere
